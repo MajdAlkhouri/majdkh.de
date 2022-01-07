83fa84d8b00af27de896a454af3cce6c942e433e
MajdAlkhouri <mjodizaodiz@gmail.com>
2022-01-07 15:06:21 +0100

    added images and started to work on slide show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'majdkh.de';
+export class AppComponent implements OnInit {
+  //wie onload bei body
+  images = ['prgramming.jpg', 'programming2.jpg', 'programming3.jpg'];
+  currentImage = 0;
+  showImage = true;
+  ngOnInit() {
+    this.uptadeImage();
+  }
+
+  uptadeImage() {
+    setInterval(() => {
+      // => für funktionen in angular
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
 }
